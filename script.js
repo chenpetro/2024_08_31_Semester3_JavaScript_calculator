d529efdf8c3e4608afd92a9b2c28749c75249a00
chenpetro <chenpetro@gmail.com>
2024-08-31 21:45:04 +0200

    lesson

---
+++ script.js
@@ -0,0 +1,55 @@
+// alert (`Use our calculator`)
+// const number1 = Number(prompt('Enter Number1'))
+// const number2 = Number(prompt('Enter Number2'))
+// console.log(number1 * number2)
+// alert(number1 * number2)
+
+// const myName = "Petro"
+// console.log(myName);
+
+// console.log(typeof number1, typeof number2);
+
+const age = 41
+console.log(age)
+
+const myName = "Petro"
+console.log(myName)
+
+isStudent = false
+console.log(isStudent)
+
+myString = `If today were the last day of my life, would I want to do what I am about to do today?`
+console.log(myString)
+
+let myNumber = 777
+myNumber = myNumber + 10
+console.log(myNumber)
+
+const myNull = null
+console.log(myNull)
+
+const enterName = prompt('Enter your name')
+alert(`Hello ${enterName}`)
+
+if (confirm("Press a button!") == true) {
+  alert (`Thanks for the confirmation!`);
+} else {
+    alert (`Action canceled!`);
+}
+
+alert (`You want to enter a secret site`)
+if (confirm("Action is dangerous. Do you want to confirm?") == true) {
+    alert (`Thanks for the confirmation!`);
+  } else {
+      alert (`Action canceled!`);
+  }
+
+//Створити змінну з іменем age і присвоїти їй значення свого віку. Вивести значення цієї змінної в консоль.
+//Створити змінну з іменем name і присвоїти їй значення свого імені. Вивести значення цієї змінної в консоль.
+//Створити змінну з іменем isStudent і присвоїти їй значення true або false, залежно від того, чи є студентом. Вивести значення цієї змінної в консоль.
+//Створити змінну з іменем myString і присвоїти їй рядок з вашої улюбленою цитатою. Вивести цей рядок в консоль.
+//Створити змінну з іменем myNumber і присвоїти їй довільне числове значення. Потім присвоїти цій змінній результат додавання до неї числа 10. Вивести значення myNumber в консоль.
+//Створити змінну з іменем myNull і присвоїти їй значення null. Вивести значення цієї змінної в консоль.
+//Створити скрипт який виводить спливаюче вікно з запитом на введення імені користувача за допомогою prompt(). Після введення імені вивести повідомлення привітання з використанням введеного імені.
+//Створити скрипт який виводить спливаюче підтвердження за допомогою confirm(). Якщо користувач підтверджує дію, вивести повідомлення "Дякую за підтвердження!". Якщо користувач відмовляється, вивести повідомлення "Дію відмінено!".
+//Створити скрипт який виводить спливаюче попередження за допомогою alert(). Вивести повідомлення про те, що дія небезпечна та попросити користувача підтвердити дію за допомогою confirm(). Якщо користувач підтверджує дію, вивести повідомлення "Дякую за підтвердження!". Якщо користувач відмовляється, вивести повідомлення "Дію відмінено!".
